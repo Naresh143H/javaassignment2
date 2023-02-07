2131cb46615e088dd7931625a767ea97e8f3918b
Naresh143H <nareshyarraa143@gmail.com>
2023-02-07 21:09:26 +0530

    compareto

---
+++ stringcompareto.java
@@ -0,0 +1,13 @@
+public class stringcompareto {
+    public static void main(String args[]){
+        String s1="naresh";
+        String s2="mani";
+        String s3="hello";
+        String s4="hai";
+        String s5="hii";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+        System.out.println(s1.compareTo(s5));
+    }
+}
