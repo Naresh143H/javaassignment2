385ef49d968c80a3719c673af68f1de1a9e3bd78
Naresh143H <nareshyarraa143@gmail.com>
2023-02-07 22:35:04 +0530

    updates value
    intial commit

---
+++ stringreplace.java
@@ -0,0 +1,7 @@
+public class stringreplace {
+    public static void main(String args[]){
+        String s1="my name is narri my name is java";
+        String replaceString=s1.replace("is","was");
+        System.out.println(replaceString);
+    }
+}
